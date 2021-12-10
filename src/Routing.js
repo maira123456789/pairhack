3559e66455a3f0c34b96471094ae03da72f5adec
M Uzakbekov <muzakbekov@MacBook-Air-M.local>
2021-12-10 16:58:54 +0600

    prepared

---
+++ src/Routing.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { Route, Routes } from "react-router";
+import Auth from "./components/Auth/Auth";
+import BrandsList from "./components/BrandsList/BrandsList";
+import Cart from "./components/Cart/Cart";
+import DetailsProduct from "./components/DetailsProduct/DetailsProduct";
+import EditProduct from "./components/EditProduct/EditProduct";
+import Home from "./components/Home/Home";
+import ProductsList from "./components/ProductsList/ProductsList";
+import AdminPage from "./pages/AdminPage";
+import Error404 from "./pages/Error404";
+
+const Routing = () => {
+  let PUBLIC_ROUTES = [
+    {
+      link: "/",
+      element: <Home />,
+      id: 1,
+    },
+    {
+      link: "/products",
+      element: <ProductsList />,
+      id: 2,
+    },
+    {
+      link: "/auth",
+      element: <Auth />,
+      id: 3,
+    },
+    {
+      link: "/brands",
+      element: <BrandsList />,
+      id: 4,
+    },
+    {
+      link: "/products/:id",
+      element: <DetailsProduct />,
+      id: 5,
+    },
+    {
+      link: "/cart",
+      element: <Cart />,
+      id: 6,
+    },
+  ];
+  const ADMIN_ROUTES = [
+    {
+      link: "/admin",
+      element: <AdminPage />,
+      id: 1,
+    },
+    {
+      link: "/edit/:id",
+      element: <EditProduct />,
+      id: 2,
+    },
+  ];
+  return (
+    <Routes>
+      {PUBLIC_ROUTES.map((item) => (
+        <Route path={item.link} element={item.element} />
+      ))}
+      {ADMIN_ROUTES.map((item) => (
+        <Route path={item.link} element={item.element} />
+      ))}
+      <Route path="*" element={<Error404/>}/>
+    </Routes>
+  );
+};
+
+export default Routing;
---
+++ src/components/EditProduct/EditProduct.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EditProduct = () => {
+    return (
+        <div>
+            EP
+        </div>
+    );
+};
+
+export default EditProduct;
