9e6a1598f5ed69aadf4a8706402e648ecb48d336
tarieltairov <tarieltairov1@gmail.com>
2021-12-11 14:29:17 +0600

    added auth

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { Link, useLocation } from "react-router-dom";
-import './Header.css'
+import "./Header.css";
+import { useAuth } from "../../context/authContext";
+import { Badge } from "@mui/material";
 
 const Header = () => {
   const location = useLocation();
+
+  const {
+    handleLogout,
+    user: { email },
+  } = useAuth();
+
   const NAV_ITEMS = [
     {
       title: "BRANDS",
@@ -29,13 +37,17 @@ const Header = () => {
   ];
   return (
     <div className="navbar">
-        <Link to="/">
-        <img  width="100px" src="https://www.svgimages.com/svg-image/s5/floral-letter-p.svg" alt="logo" />
-        </Link>
+      <Link to="/">
+        <img
+          width="100px"
+          src="https://www.svgimages.com/svg-image/s5/floral-letter-p.svg"
+          alt="logo"
+        />
+      </Link>
       {NAV_ITEMS.map((item) => (
         <Link
           to={item.link}
-          style={{textDecoration: 'none'}}
+          style={{ textDecoration: "none" }}
           className={
             location.pathname === item.link
               ? "navbar__item-active"
@@ -45,7 +57,40 @@ const Header = () => {
           {item.title}
         </Link>
       ))}
-      <ShoppingCartIcon />
+
+      {email === "tarieltairov1@gmail.com" ? (
+        <Link
+          className={
+            location.pathname === "/admin"
+              ? "navbar__item-active"
+              : "navbar__item"
+          }
+          to="/admin"
+          style={{ textDecoration: "none" }}
+        >
+          ADMIN
+        </Link>
+      ) : null}
+      <Link to="/cart">
+        <Badge badgeContent={4} color="secondary">
+          <ShoppingCartIcon className="cart"  fontSize="large"/>
+        </Badge>
+      </Link>
+      <div>
+        {email ? (
+          <Link to="/auth">
+            <button className="sign-btn" onClick={handleLogout}>
+              Logout
+            </button>
+          </Link>
+        ) : null}
+
+        {email ? null : (
+          <Link to="/auth">
+            <button className="sign-btn">Login</button>
+          </Link>
+        )}
+      </div>
     </div>
   );
 };
