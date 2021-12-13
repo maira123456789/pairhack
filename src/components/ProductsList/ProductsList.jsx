4825e6e3078200e2a1e9cb3cac2569834e460fa1
M Uzakbekov <muzakbekov@MacBook-Air-M.local>
2021-12-13 18:49:05 +0600

    search and filter

--- src/components/ProductsList/ProductsList.jsx
+++ src/components/ProductsList/ProductsList.jsx
@@ -4,9 +4,12 @@ import ProductsCard from "./ProductsCard";
 import ErrorIcon from "@mui/icons-material/Error";
 import "./ProductList.css";
 import { useSearchParams } from "react-router-dom";
-import { Pagination } from "@mui/material";
+import { Pagination, TextField } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import Filters from "../Filters/Filters";
 const ProductsList = () => {
-  const { perfumes, getPerfumes, perfumesTotalCount } = useContext(perfumesContext);
+  const { perfumes, getPerfumes, perfumesTotalCount } =
+    useContext(perfumesContext);
   useEffect(() => {
     getPerfumes();
   }, []);
@@ -17,33 +20,57 @@ const ProductsList = () => {
     searchParams.get("_page") ? searchParams.get("_page") : 1
   );
 
-  const [limit, setLimit] = useState(
-    4
+  const [limit, setLimit] = useState(4);
+
+  const pageCount = Math.ceil(perfumesTotalCount / limit);
+  console.log(searchParams.get("_limit"));
+
+  const [search, setSearch] = useState(
+    searchParams.get("q") ? searchParams.get("q") : ""
   );
 
-  const pageCount = Math.ceil(perfumesTotalCount/limit)
-  console.log(searchParams.get('_limit'))
+  const [brand,setBrand] = useState([])
 
   useEffect(() => {
     setSearchParams({
       _page: page,
       _limit: limit,
+      q: search,
+      brand: brand
+      
     });
   }, []);
-
+//   console.log(brand);
   useEffect(() => {
     setSearchParams({
       _page: page,
       _limit: limit,
+      q: search,
+      brand: brand
     });
-  }, [page, limit]);
+  }, [page, limit, search, brand]);
   useEffect(() => {
     getPerfumes();
   }, [searchParams]);
 
   return (
     <div className="DIV">
-      <div style={{ width: "100%", height: "100px" }}></div>
+        {/* search */}
+      <div style={{display:"flex",alignItems:"center"}}>
+        <SearchIcon />
+        <TextField
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Search..."
+        />
+      </div>
+      {/* filtration */}
+      <div>
+          <Filters
+          brand={brand}
+          setBrand={setBrand}
+          />
+      </div>
       <div
         style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
       >
@@ -53,18 +80,17 @@ const ProductsList = () => {
           <ErrorIcon />
         )}
       </div>
-      <div style={{display: 'flex', justifyContent: 'center'}}>
+      <div style={{ display: "flex", justifyContent: "center" }}>
         <Pagination
           onChange={(event, page) => {
             setPage(page);
-            
           }}
           page={+page}
           pageSize={+limit}
           defaultPage={1}
           count={+pageCount}
           color="warning"
-        //   variant="outlined"
+          //   variant="outlined"
         />
       </div>
     </div>
