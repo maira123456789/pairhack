866bb51485603b01106a25f510374354ba69de34
M Uzakbekov <muzakbekov@MacBook-Air-M.local>
2021-12-13 13:17:55 +0600

    configurated

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -2,10 +2,12 @@ import { Box, Button, Modal, TextField } from "@mui/material";
 import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { perfumesContext } from "../context/perfumesContext";
+import { brandsContext } from "../context/brandsContext";
 import "./AdminPage.css";
 
 const AdminPage = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const { createBrand } = useContext(brandsContext);
   const { createPerfume } = useContext(perfumesContext);
   const [newPerfume, setNewPerfume] = useState({
     brand: "",
@@ -17,34 +19,62 @@ const AdminPage = () => {
     male: "",
   });
 
-  function handleValues(e){
-    let values ={
+  const [newBrand, setNewBrand] = useState({
+    brand: "",
+    logo: "",
+  });
+
+  function handleValues(e) {
+    let values = {
       ...newPerfume,
-      [e.target.name]:e.target.value
-    }
-    setNewPerfume(values)
+      [e.target.name]: e.target.value,
+    };
+    setNewPerfume(values);
+  }
+
+  function handleValuesBrand(e) {
+    let values = {
+      ...newBrand,
+      [e.target.name]: e.target.value,
+    };
+    setNewBrand(values);
   }
 
   function checkValues() {
     if (
       !newPerfume.brand ||
       !newPerfume.model ||
-      !newPerfume.description||
-      !newPerfume.price||
-      !newPerfume.image1||
-      !newPerfume.image2||
+      !newPerfume.description ||
+      !newPerfume.price ||
+      !newPerfume.image1 ||
+      !newPerfume.image2 ||
       !newPerfume.male
     ) {
       alert("Please, fill in all fields!");
       return;
     } else {
-        createPerfume(newPerfume);
-        navigate("/products")
+      createPerfume(newPerfume);
+      navigate("/products");
     }
-}
+  }
+
+  function checkValuesBrand() {
+    if (!newBrand.brand || !newBrand.logo) {
+      alert("Please, fill in all fields!");
+      return;
+    } else {
+      createBrand(newBrand);
+      navigate("/brands");
+    }
+  }
+
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  const [openBrand, setOpenBrand] = useState(false);
+  const handleOpenBrand = () => setOpenBrand(true);
+  const handleCloseBrand = () => setOpenBrand(false);
 
   const style = {
     position: "absolute",
@@ -63,77 +93,115 @@ const AdminPage = () => {
     textAlign: "center",
   };
   return (
-    <div style={{ paddingTop: "210px", paddingBottom: "210px" }}>
-      <Button onClick={handleOpen} variant="contained">
-        Add Perfume
-      </Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <h1>Add Perfumes</h1>
-          <TextField
-            label="Enter brand"
-            variant="outlined"
-            className="inp-add"
-            onChange={handleValues}
-            name="brand"
-          />
-          <TextField
-            label="Enter model"
-            variant="outlined"
-            className="inp-add"
-            onChange={handleValues}
-            name="model"
-          />
-          <TextField
-            label="Enter description"
-            variant="outlined"
-            className="inp-add"
-            onChange={handleValues}
-            name="description"
-          />
-          <TextField
-            label="Enter price"
-            variant="outlined"
-            className="inp-add"
-            onChange={handleValues}
-            name="price"
-            type="number"
-          />
-          <TextField
-            label="Enter url for image1"
-            variant="outlined"
-            className="inp-add"
-            onChange={handleValues}
-            name="image1"
-          />
-          <TextField
-            label="Enter url for image2"
-            variant="outlined"
-            className="inp-add"
-            onChange={handleValues}
-            name="image2"
-          />
-          <TextField
-            label="Enter male"
-            variant="outlined"
-            className="inp-add"
-            onChange={handleValues}
-            name="male"
-          />
-          <Button
-            variant="contained"
-            style={{ width: "150px", height: "40px" }}
-            onClick={()=>checkValues()}
-          >
-            Add
-          </Button>
-        </Box>
-      </Modal>
+    <div className="admin" style={{display:"flex", justifyContent:"center", alignItems:"center",paddingTop: "310px", paddingBottom: "210px"}}>
+      <div style={{margin:"10px" }}>
+        <Button onClick={handleOpen} variant="contained" style={{backgroundColor:"honeydew",color:"black"}}>
+          Add Perfume
+        </Button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <h1>Add Perfumes</h1>
+            <TextField
+              label="Enter brand"
+              variant="outlined"
+              className="inp-add"
+              onChange={handleValues}
+              name="brand"
+            />
+            <TextField
+              label="Enter model"
+              variant="outlined"
+              className="inp-add"
+              onChange={handleValues}
+              name="model"
+            />
+            <TextField
+              label="Enter description"
+              variant="outlined"
+              className="inp-add"
+              onChange={handleValues}
+              name="description"
+            />
+            <TextField
+              label="Enter price"
+              variant="outlined"
+              className="inp-add"
+              onChange={handleValues}
+              name="price"
+              type="number"
+            />
+            <TextField
+              label="Enter url for image1"
+              variant="outlined"
+              className="inp-add"
+              onChange={handleValues}
+              name="image1"
+            />
+            <TextField
+              label="Enter url for image2"
+              variant="outlined"
+              className="inp-add"
+              onChange={handleValues}
+              name="image2"
+            />
+            <TextField
+              label="Enter male"
+              variant="outlined"
+              className="inp-add"
+              onChange={handleValues}
+              name="male"
+            />
+            <Button
+              variant="contained"
+              style={{ width: "150px", height: "40px" }}
+              onClick={() => checkValues()}
+            >
+              Add
+            </Button>
+          </Box>
+        </Modal>
+      </div>
+      <div>
+        <Button onClick={handleOpenBrand} variant="contained" style={{backgroundColor:"mistyrose",color:"black"}}>
+          Add Brand
+        </Button>
+        <Modal
+          open={openBrand}
+          onClose={handleCloseBrand}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <h1>Add Brand</h1>
+            <TextField
+              label="Enter brand"
+              variant="outlined"
+              className="inp-add"
+              onChange={handleValuesBrand}
+              name="brand"
+            />
+            <TextField
+              label="Enter logo URL"
+              variant="outlined"
+              className="inp-add"
+              onChange={handleValuesBrand}
+              name="logo"
+            />
+            <Button
+              variant="contained"
+              style={{ width: "150px", height: "40px" }}
+              onClick={() => checkValuesBrand()}
+            >
+              Add
+            </Button>
+          </Box>
+        </Modal>
+      </div>
     </div>
   );
 };
